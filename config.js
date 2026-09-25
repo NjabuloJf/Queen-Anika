325e61c719a07d001c48f76562c6c246716c31b5
Njabulo Jb <fanajb65@gmail.com>
2026-09-26 01:52:04 +0200

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,7 @@ module.exports = {
     ALIVE_MSG: process.env.ALIVE_MSG || "Hey there, I'm alive",
     OWNER_NUMBER: process.env.OWNER_NUMBER || "26773968411",
     PRO_USERS: process.env.PRO_USERS || "26777821911",
+    AUDD_API_TOKEN: process.env.AUDD_API_TOKEN || "b132bfc095eb6baf79e759c7f0f981b3",
     MODE: process.env.MODE || "public",
     PREFIX: process.env.PREFIX || ".",
     BOT_NAME: process.env.BOT_NAME || "Queen-Anika",
