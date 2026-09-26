2ad1afb1a3733bef9ef1a041755a6c9079fc114c
Njabulo Jb <fanajb65@gmail.com>
2026-09-26 04:34:07 +0200

    Update gstatus.js

--- plugins/gstatus.js
+++ plugins/gstatus.js
@@ -10,7 +10,7 @@ const {
 } = require("@whiskeysockets/baileys");
 
 // ========== BRANDING IMAGE ==========
-const BRAND_IMAGE = "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/Queen-Anika.png";
+const BRAND_IMAGE = "https://raw.githubusercontent.com/NjabuloJf/njabulo-data/main/njabuloimg/njabuloimg3.png";
 
 // ========== CONTEXT INFO ==========
 function ctxInfo() {
@@ -18,7 +18,7 @@ function ctxInfo() {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '1203634129500689311@newsletter',
+            newsletterJid: '120363402336733732@newsletter',
             newsletterName: 'Queen-Anika'
         }
     };
